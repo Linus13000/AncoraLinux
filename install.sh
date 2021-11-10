e917220903cb7c8b2f2d3bb1a6d8b2f0bb050fac
Linus13000 <65447448+Linus13000@users.noreply.github.com>
2021-11-11 00:37:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,18 +15,3 @@ fdisk $disk
 mkfs.ext4 ${disk}1
 mount ${disk}1 /mnt
 pacstrap /mnt base linux linux-firmware grub vim iwd dhcpcd sudo amd-ucode intel-ucode xorg mesa xf86-video-intel lxde base-devel git python
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-hwclock --systohc
-echo en_US.UTF-8 >> /etc/locale.gen
-echo de_DE.UTF-8 >> /etc/locale.gen
-locale-gen
-echo de_DE.UTF-8 >> /etc/locale.conf
-echo KEYMAP=de-latin1 >> /etc/vconsole.conf
-echo AncoraLinux >> /etc/hostname
-passwd
-echo GRUB_DISABLE_OS_PROBER=false >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-echo "Now reboot"
-exit
